4eb4afb348ac6c1d562414b347dfb199fa9ad5c5
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-04-09 18:19:31 +0200

    Try to use 'generic' allocator by enabling some macros

--- src/strings.h
+++ src/strings.h
@@ -12,11 +12,27 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#ifndef GRRRS_MALLOC
+#define GRRRS_MALLOC(...) malloc(__VA_ARGS__)
+#endif
+
+#ifndef GRRRS_REALLOC
+#define GRRRS_REALLOC(A, ...) realloc((A), __VA_ARGS__)
+#endif
+
+#ifndef GRRRS_FREE
+#define GRRRS_FREE(A) free(A)
+#endif
+
+#ifndef GRRRS_OOM
+#define GRRRS_OOM
+#endif
+
 #define internal static
 #define _VOID(A) (NULL == (A))
 #define _OKP(A) (NULL != (A))
 
-// TODO(marius): investigate how this aligns - I suspect the 2 16bit integers
+// TODO(marius): investigate how this aligns
 struct grrr_string {
     int_fast16_t len;
     int_fast16_t cap;
@@ -42,14 +58,18 @@ void _grrrs_free(char *s)
     }
 
     if (_VOID(gs->data)) {
-        free(gs->data);
+        GRRRS_FREE(gs->data);
     }
-    free(gs);
+    GRRRS_FREE(gs);
 }
 
 struct grrr_string *_grrrs_new_empty()
 {
-    struct grrr_string *result = malloc(sizeof(struct grrr_string));
+    struct grrr_string *result = GRRRS_MALLOC(sizeof(struct grrr_string));
+    if (_VOID(result)) {
+        GRRRS_OOM;
+        return (void*)(2*sizeof(char));
+    }
 
     result->len = 0;
     result->cap = 0;
@@ -70,7 +90,11 @@ internal int __strlen(const char *s)
 struct grrr_string *_grrrs_new_from_cstring(const char* s)
 {
     int len = __strlen(s);
-    struct grrr_string *result = malloc(sizeof(struct grrr_string) + (len * sizeof(char)) + 1);
+    struct grrr_string *result = GRRRS_MALLOC(sizeof(struct grrr_string) + (len * sizeof(char)) + 1);
+    if (_VOID(result)) {
+        GRRRS_OOM;
+        return (void*)(2*sizeof(char));
+    }
 
     result->len = len;
     result->cap = len;
@@ -102,6 +126,17 @@ int grrrs_len(const char* s)
     assert(__strlen(s) == gs->len);
 #endif
     return gs->len;
+}
+
+void *_grrrs_resize(void *s, int new_cap)
+{
+    struct grrr_string *gs = _grrrs_ptr((char*)s);
+#if DDEBUG
+    assert(_OKP(gs))
+#endif
+    gs = realloc(gs, sizeof(struct grrr_string) + (new_cap * sizeof(char)) + 1);
+
+    return gs->data;
 }
 
 #define grrrs_new(A) (_VOID(A) ? \
