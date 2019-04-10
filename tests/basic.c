6955f78091edfc4a8726988c8f3eb3d30d026418
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-04-10 10:11:51 +0200

    Add asserteq_int for checking len and cap values

--- tests/basic.c
+++ tests/basic.c
@@ -14,46 +14,46 @@ describe(basic) {
     it("Zero string") {
         char *t = grrrs_new(0);
 
-        assert(grrrs_len(t) == 0);
-        assert(grrrs_cap(t) == 0);
-        asserteq_buf(t, "", 1, "Different strings");
+        asserteq_int(grrrs_len(t), 0);
+        asserteq_int(grrrs_cap(t), 0);
+        asserteq_buf(t, "", 1);
         _grrrs_free(t);
     }
 
     it("Check pointer addresses") {
         char *t = grrrs_new(0);
 
-        assert(grrrs_len(t) == 0);
-        assert(grrrs_cap(t) == 0);
-        asserteq_buf(t, "", 1, "Different strings");
+        asserteq_int(grrrs_len(t), 0);
+        asserteq_int(grrrs_cap(t), 0);
+        asserteq_buf(t, "", 1);
         struct grrr_string *gs = _grrrs_ptr(t);
 
-        assert((void*)&gs->data == (void*)t);
-        assert((void*)&gs->cap == (void*)(t - sizeof(gs->cap)));
-        assert((void*)&gs->len == (void*)(t - sizeof(gs->cap) - sizeof(gs->len)));
-        assert((void*)gs == (void*)(t - sizeof(gs->cap) - sizeof(gs->len)));
+        asserteq_ptr((void*)&gs->data, (void*)t);
+        asserteq_ptr((void*)&gs->cap, (void*)(t - sizeof(gs->cap)));
+        asserteq_ptr((void*)&gs->len, (void*)(t - sizeof(gs->cap) - sizeof(gs->len)));
+        asserteq_ptr((void*)gs, (void*)(t - sizeof(gs->cap) - sizeof(gs->len)));
 
-        assert(gs->len == 0);
-        assert(gs->cap == 0);
+        asserteq_int(gs->len, 0);
+        asserteq_int(gs->cap, 0);
 
         _grrrs_free(t);
     }
 
     it("From C string") {
         char *t = grrrs_new("ana are mere\n");
-        assert(grrrs_len(t) == 13);
-        assert(grrrs_cap(t) == 13);
+        asserteq_int(grrrs_len(t), 13);
+        asserteq_int(grrrs_cap(t), 13);
         asserteq_buf(t, "ana are mere\n", 14, "Different strings");
         _grrrs_free(t);
     }
 
     it("Resize string") {
         char *t = grrrs_new(0);
-        assert(grrrs_len(t) == 0);
-        assert(grrrs_cap(t) == 0);
+        asserteq_int(grrrs_len(t), 0);
+        asserteq_int(grrrs_cap(t), 0);
 
         t = _grrrs_resize(t, 128);
-        assert(grrrs_cap(t) == 128);
+        asserteq_int(grrrs_cap(t), 128);
         _grrrs_free(t);
     }
 }
